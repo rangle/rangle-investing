76d28fa0c8a9dd92ce3f04cf68f66a0768e38cc3
Baggio Wong <baggio.wong@rangle.io>
2018-05-09 11:03:37 -0400

    Add StockService dependency to imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StocksService } from './services/stocks.service';
 
 
 @NgModule({
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ StocksService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
