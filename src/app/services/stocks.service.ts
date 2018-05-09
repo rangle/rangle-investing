fd7f8729e18c1088afc1f1f2d4768d39846191b8
Baggio Wong <baggio.wong@rangle.io>
2018-05-09 11:44:43 -0400

    Add comments to StocksService methods.

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -6,12 +6,21 @@ import { Observable } from 'rxjs';
 export class StocksService {
 	constructor() { }
 
+	/**
+	 *	@param {string} tickerSymbol Stock ticker symbol
+	 *	@return {boolean} Whether the stock exists or not
+	 */
 	private static _stockExists(tickerSymbol: string): boolean {
 		if (mockStocksData[tickerSymbol] === true) return true;
 
 		return false;
 	}
 
+	/**
+	 *	@param {string} tickerSymbol Stock ticker symbol
+	 *	@return {Observable<any>} Returns an Observable that emits
+	 *	stock quote once and completes. 
+	 */
 	getQuote(tickerSymbol: string): Observable<any> {
 		return new Observable((observer) => {
 			const { next, error, complete } = observer;
@@ -29,6 +38,11 @@ export class StocksService {
 		});
 	}
 
+	/**
+	 *	@param {string} tickerSymbol Stock ticker symbol
+	 *	@return {Observable<any>} Returns an Observable that emits
+	 *	stock chart once and completes. 
+	 */
 	getChart(tickerSymbol: string): Observable<any> {
 		return new Observable((observer) => {
 			const { next, error, complete } = observer;
@@ -46,6 +60,12 @@ export class StocksService {
 		});
 	}
 
+	/**
+	 *	@param {string} tickerSymbol Stock ticker symbol
+	 *	@param {string} dateString Date in this format "yyyy-mm-dd"
+	 *	@return {Observable<any>} Returns an Observable that emits
+	 *	stock chart at specified date once and completes. 
+	 */
 	getChartByDate(tickerSymbol: string, dateString: string): Observable<any> {
 		return new Observable((observer) => {
 			const { next, error, complete } = observer;
