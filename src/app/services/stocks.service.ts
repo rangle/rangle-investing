891dcb9b174c02d44bf23a09e358b63ff8195e32
Baggio Wong <baggio.wong@rangle.io>
2018-05-09 11:40:55 -0400

    Add initial methods to StocksService.
    Methods available to be called: getQuote, getChart, getChartByDate

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
 import { mockStocksData } from '../data/finance-data';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class StocksService {
+	constructor() { }
 
+	private static _stockExists(tickerSymbol: string): boolean {
+		if (mockStocksData[tickerSymbol] === true) return true;
 
-  constructor() { }
+		return false;
+	}
+
+	getQuote(tickerSymbol: string): Observable<any> {
+		return new Observable((observer) => {
+			const { next, error, complete } = observer;
+
+			if (StocksService._stockExists(tickerSymbol)) {
+				if (mockStocksData[tickerSymbol]["quote"]) {
+					next(mockStocksData[tickerSymbol]["quote"]);
+					complete();
+				} else {
+					error("Error retrieving quote data.");
+				}	
+			} else {
+				error(`Stock by ticker symbol ${tickerSymbol} doesn't seem to exist.`);
+			}
+		});
+	}
+
+	getChart(tickerSymbol: string): Observable<any> {
+		return new Observable((observer) => {
+			const { next, error, complete } = observer;
+
+			if (StocksService._stockExists(tickerSymbol)) {
+				if (mockStocksData[tickerSymbol]["chart"]) {
+					next(mockStocksData[tickerSymbol]["chart"]);
+					complete();
+				} else {
+					error("Error retrieving chart data.");
+				}
+			} else {
+				error(`Stock by ticker symbol ${tickerSymbol} doesn't seem to exist.`);
+			}
+		});
+	}
+
+	getChartByDate(tickerSymbol: string, dateString: string): Observable<any> {
+		return new Observable((observer) => {
+			const { next, error, complete } = observer;
+
+			if (StocksService._stockExists(tickerSymbol)) {
+				if (mockStocksData[tickerSymbol]["chart"]) {
+					let chartAtDate = mockStocksData[tickerSymbol]["chart"][dateString];
+					if (chartAtDate) {
+						next(chartAtDate);
+						complete();
+					} else {
+						error("Cannot find chart entry for specified date.");
+					}
+				} else {
+					error("Error retrieving chart data.");
+				}
+			} else {
+				error(`Stock by ticker symbol ${tickerSymbol} doesn't seem to exist.`);
+			}
+		});
+	}
 }
