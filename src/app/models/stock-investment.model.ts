19865d61113611bfc2759c871ffe5fa6a21f835d
Baggio Wong <baggio.wong@rangle.io>
2018-05-11 13:19:43 -0400

    Change StockInvestment class to interface, delete constructor.

--- src/app/models/stock-investment.model.ts
+++ src/app/models/stock-investment.model.ts
@@ -1,15 +1,8 @@
 import { StockTransaction } from "./stock-transaction.model";
 
-export class StockInvestment {
+export interface StockInvestment {
 	symbol: string;
 	amountOfStock: number;
 	currentValue: number;
 	transactions: Array<StockTransaction>;
-
-	constructor(obj?: any) {
-		this.symbol 	= obj && obj.symbol 				|| "";
-		this.amountOfStock		= obj && obj.amountOfStock 	|| 0;
-		this.currentValue		= obj && obj.currentValue 	|| 0;		
-		this.transactions		= obj && obj.price 			|| new Array();
-	}
 }
