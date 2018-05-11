21a13f767ebbda016dbe0cb03248f7cd101accc1
Baggio Wong <baggio.wong@rangle.io>
2018-05-11 18:00:02 -0400

    Add comment in case source stocks data changes.

--- src/app/store/reducers/stocks.ts
+++ src/app/store/reducers/stocks.ts
@@ -4,7 +4,8 @@ import * as stocks from '../actions/stocks';
 export function reducer(state: any[] = [], action: stocks.LoadAction) {
   switch (action.type) {
     case stocks.LOAD:
-    	if (state === undefined || state.length === 0) {
+    	// this only works because our stocks data is static, if it's not static, this needs to be replaced, not appended
+    	if (state === undefined || state.length === 0) { // only append when loading for the first time
     		return [...state, ...action.payload];		
     	} else {
     		return state;
