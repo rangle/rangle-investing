26e0336bbc6dc26ce0eedc78264f0aaaf6551870
Baggio Wong <baggio.wong@rangle.io>
2018-05-11 16:42:50 -0400

    Add StockItem component.

---
+++ src/app/components/stock-item/stock-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-item',
+  templateUrl: './stock-item.component.html',
+  styleUrls: ['./stock-item.component.css']
+})
+export class StockItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
