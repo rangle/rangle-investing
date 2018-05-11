35afe682d07f54b7a745bbbe41c882a8a653ef62
Baggio Wong <baggio.wong@rangle.io>
2018-05-11 17:54:05 -0400

    Configure StockSummary component.

--- src/app/components/stock-summary/stock-summary.component.ts
+++ src/app/components/stock-summary/stock-summary.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store';
+import { LoadAction as StocksLoadAction } from '../../store/actions/stocks';
+import { mockStocksData } from '../../data/finance-data';
 
 @Component({
   selector: 'app-stock-summary',
@@ -6,8 +12,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-summary.component.css']
 })
 export class StockSummaryComponent implements OnInit {
-
-  constructor() { }
+	stockSymbol: string;
+	allStocks: any[];
+	stock: any;
+	constructor(private route: ActivatedRoute,
+				private store: Store<AppState>) { 
+		// get stock symbol, get stocks from store, get stock data
+		route.params.subscribe(params => this.stockSymbol = params['stockSymbol']);
+		this.store.select('stocks').subscribe( (stocks: any[]) => this.allStocks = [...stocks] );
+		this.stock = this.allStocks.filter(stock => stock.name === this.stockSymbol).pop()["quote"];
+	}
 
   ngOnInit() {
   }
