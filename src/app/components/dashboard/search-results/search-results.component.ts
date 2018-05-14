ecdc76b510c104decff693fde0862c192f994654
Baggio Wong <baggio.wong@rangle.io>
2018-05-14 15:29:09 -0400

    Lint fixes for search-results.component.ts.

--- src/app/components/dashboard/search-results/search-results.component.ts
+++ src/app/components/dashboard/search-results/search-results.component.ts
@@ -6,24 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-results.component.css']
 })
 export class SearchResultsComponent implements OnInit {
-	mockSearchResults = [
-		{
-			stockSymbol: "AAPL",
-			inWatchlist: false,
-			currency: "USD",
-			stockPrice: 189.26,
-			stockPriceChange: 0.67,
-		},
-		{
-			stockSymbol: "MSFT",
-			inWatchlist: false,
-			currency: "USD",
-			stockPrice: 98.23,
-			stockPriceChange: 0.56,
-		}
-	];
-	searchResultsCount = this.mockSearchResults.length;
-	
+  mockSearchResults = [
+    {
+      stockSymbol: 'AAPL',
+      inWatchlist: false,
+      currency: 'USD',
+      stockPrice: 189.26,
+      stockPriceChange: 0.67,
+    },
+    {
+      stockSymbol: 'MSFT',
+      inWatchlist: false,
+      currency: 'USD',
+      stockPrice: 98.23,
+      stockPriceChange: 0.56,
+    }
+  ];
+  searchResultsCount = this.mockSearchResults.length;
+
   constructor() { }
 
   ngOnInit() {
