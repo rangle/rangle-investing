becb68fd6f5ed92385ea4519cb29cad2699affca
Baggio Wong <baggio.wong@rangle.io>
2018-05-14 15:48:06 -0400

    Lint fixes for stock.model.ts.

--- src/app/models/stock.model.ts
+++ src/app/models/stock.model.ts
@@ -1,14 +1,14 @@
 export interface Stock {
-	symbol: string;
-	companyName: string;
-	primaryExchange: string;
-	open: number;
-	close: number;
-	avgTotalVolume: number;
-	week52High: number;
-	week52Low: number;
-	iexVolume: number;
-	marketCap: number;
-	peRatio: number;
-	latestPrice: number;
+  symbol: string;
+  companyName: string;
+  primaryExchange: string;
+  open: number;
+  close: number;
+  avgTotalVolume: number;
+  week52High: number;
+  week52Low: number;
+  iexVolume: number;
+  marketCap: number;
+  peRatio: number;
+  latestPrice: number;
 }
