d47d7a9addfd9c1ba64bcd29ce3169587570bece
Nikhil Metrani <nikhilmetrani@users.noreply.github.com>
2018-05-16 10:59:22 -0400

    Implementaton for funds add and withdraw actions and reducer
    Each add / withdraw action is recorder into transactions entry of the state.

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -7,9 +7,11 @@ export interface AppState {
 import { reducer as stocksReducer } from './reducers/stocks';
 import { reducer as watchlistReducer } from './reducers/watchlist';
 import { reducer as routerUrlReducer } from './reducers/navigation';
+import { reducer as fundsReducer } from './reducers/funds';
 
 export const reducers = {
   stocks: stocksReducer,
   watchlist: watchlistReducer,
   routerUrl: routerUrlReducer,
+  funds: fundsReducer,
 };
---
+++ src/app/store/reducers/funds.spec.ts
@@ -0,0 +1,33 @@
+import * as funds from '../actions/funds';
+import { FundsState, FundTransaction, FundTransactionTypes, reducer } from './funds';
+
+describe('Funds Reducer', () => {
+  describe('Default Action', () => {
+    it('should return the default state', () => {
+      const action = {} as any;
+      const result = reducer(undefined, action);
+      expect(result.totalAmount).toEqual(0);
+      expect(result.transactions.length).toEqual(0);
+    });
+  });
+  describe('Add Action', () => {
+    it('should return new state with extra amount', () => {
+      const state: FundsState = { transactions: [], totalAmount: 0};
+      const actionS1 = new funds.AddAction(1200);
+      const result = reducer(state, actionS1);
+      expect(result.transactions.length).toEqual(1);
+      expect(result.transactions[0].amount).toEqual(1200);
+      expect(result.totalAmount).toEqual(1200);
+    });
+  });
+  describe('Withdraw Action', () => {
+    it('should return new state substracting given amount', () => {
+      const state: FundsState = { transactions: [], totalAmount: 0};
+      let result = reducer(state, new funds.AddAction(1200));
+      result = reducer(result, new funds.WithdrawAction(200));
+      expect(result.transactions.length).toEqual(2);
+      expect(result.transactions[1].amount).toEqual(200);
+      expect(result.totalAmount).toEqual(1000);
+    });
+  });
+});
