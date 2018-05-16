620adb37996492dfb03f408d822d860dac1105a6
Baggio Wong <baggio.wong.hk@gmail.com>
2018-05-16 15:20:45 -0400

    Add Portfolio component.
    This only includes only Portfolio screen in this card
    https://trello.com/c/1QGEZ4a3/.

--- src/app/containers/portfolio-page/portfolio-page.component.spec.ts
+++ src/app/containers/portfolio-page/portfolio-page.component.spec.ts
@@ -1,8 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { PortfolioComponent } from '../../components/portfolio/portfolio.component';
 import { PortfolioPageComponent } from './portfolio-page.component';
 import { NavbarItemComponent } from '../../components/navbar-item/navbar-item.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from '../../store';
 
 describe('PortfolioPageComponent', () => {
   let component: PortfolioPageComponent;
@@ -10,7 +13,11 @@ describe('PortfolioPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PortfolioPageComponent ]
+      imports: [ StoreModule.forRoot(reducers) ],
+      declarations: [
+        PortfolioPageComponent,
+        PortfolioComponent
+      ]
     })
     .compileComponents();
   }));
