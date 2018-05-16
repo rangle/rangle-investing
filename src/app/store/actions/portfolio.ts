464662da006aa6e0eb07951c1be3eef17e512695
Nikhil Metrani <nikhilmetrani@users.noreply.github.com>
2018-05-16 15:35:46 -0400

    Fix lint errors

--- src/app/store/actions/portfolio.ts
+++ src/app/store/actions/portfolio.ts
@@ -8,16 +8,16 @@ export interface PortfolioPayload {
   symbol: string;
   units: number;
   pricePerUnit: number;
-};
+}
 
 export class BuyAction implements Action {
   readonly type = BUY;
   constructor(public payload: PortfolioPayload) { }
-};
+}
 
 export class SellAction implements Action {
   readonly type = SELL;
   constructor(public payload: PortfolioPayload) { }
-};
+}
 
 export type Actions = BuyAction | SellAction;
