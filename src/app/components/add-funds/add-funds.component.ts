f29b4b554cb23d9a76c382c38e1262dc381a9493
Baggio Wong <baggio.wong@rangle.io>
2018-05-22 14:45:26 -0400

    Fixes for addFunds.
    Changed input trigger event from click and keypress.enter to ngSubmit
    Added validation on submit.

--- src/app/components/add-funds/add-funds.component.ts
+++ src/app/components/add-funds/add-funds.component.ts
@@ -11,8 +11,23 @@ export class AddFundsComponent {
   @Input() funds;
   @Output() addFunds = new EventEmitter<string>();
   showAddFunds = false;
+  fundsAmountString = '';
+  showErrorMsg = false;
+  errorMsg = '';
 
-  addFundsHandler(amount: string) {
-    this.addFunds.emit(amount);
+  private fundsStringValid(funds: string) {
+    const fundsRegex = new RegExp(/^\d+\.\d{2}$/, 'g'); // 12312392.02
+    return fundsRegex.test(funds);
+  }
+
+  addFundsHandler() {
+    if (!this.fundsStringValid(this.fundsAmountString)) {
+      this.showErrorMsg = true;
+      this.errorMsg = 'The string must be a positive number, followed by two decimals, e.g. 1000.00';
+    } else {
+      this.showErrorMsg = false;
+      this.errorMsg = '';
+      this.addFunds.emit(this.fundsAmountString);
+    }
   }
 }
