1b0128c148b15e8367b137aab1f3be56711c54b7
Baggio Wong <baggio.wong@rangle.io>
2018-05-23 12:24:08 -0400

    Add buy error message.
    Shows error message for buy if input invalid.
    Added comments.

--- src/app/containers/stock-details-page/stock-details-page.component.ts
+++ src/app/containers/stock-details-page/stock-details-page.component.ts
@@ -20,6 +20,8 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
   currentHoldings: PortfolioStockItem[];
   purchaseCost: number;
   quantity: number;
+  buyErrorMsg = 'Please enter a whole number, e.g. 10000.';
+  showBuyError = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -27,17 +29,24 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
     private store: Store<AppState>
     ) { }
 
+  /**
+   *  @param {string} tickerSymbol
+   *  @description See if stock exists in stocks array. 
+   */
   private stockExists(tickerSymbol: string) {
     const stockIndex = this.stocks.findIndex(stock => stock.name === tickerSymbol.toUpperCase());
 
     return (stockIndex === -1) ? false : true;
   }
 
+  /**
+   *  @description Set up tickerSymbol (router param), stocks, stock, portfolio, currentHoldings, purchaseCost, quantity class variables. 
+   */
   ngOnInit() {
     const subscriptionParams = this.activatedRoute.params.subscribe(params => {
       this.tickerSymbol = params['tickerSymbol'];
 
-      // guard against direct navigation with spurious param (non-existent symbol)
+      // prevent direct navigation with non existent symbol
       if (!this.stockExists(this.tickerSymbol)) {
         this.route.navigate(['']);
       }
@@ -61,6 +70,9 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
     this.subscriptions.push(subscriptionParams);
   }
 
+  /**
+   *  @description Unsubscribe to subscriptions. 
+   */
   ngOnDestroy() {
     for (const subscription of this.subscriptions) {
       if (subscription) {
@@ -69,13 +81,22 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   *  @param {string} input
+   *  @description Checks if string is an integer. 
+   */
   private isNumber(input: string) {
     const numberRegex = new RegExp(/^\d+$/, 'g');
     return numberRegex.test(input);
   }
 
+  /**
+   *  @param {string} units Number of units of stock user wants to buy. 
+   *  @description If purchase units is an integer, dispatch buy action. Otherwise shows error message (invalid input).
+   */ 
   buy(units: string) {
     if (this.isNumber(units)) {
+      this.showBuyError = false;
       const currentStockData = this.stocks.find(stock => stock.name === this.tickerSymbol.toUpperCase());
 
       if (currentStockData && currentStockData.quote) {
@@ -87,9 +108,17 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
 
         this.store.dispatch(new BuyAction(stock));
       }
+    } else {
+      this.showBuyError = true;
     }
   }
 
+  /**
+   @param {string} id This is the id field in the PortfolioStockItem interface. Identifies stock transaction.
+   @param {number} targetUnits Number of units of stock user wants to sell. 
+   @param {string} tickerSymbol
+   @description Checks if user has enough units of stock to sell. This only checks for stocks under the provided id, which means that other transactions made for the same stock will be ignored. 
+   */
   private enoughStock(id: string, targetUnits: number, tickerSymbol: string) {
     if (this.portfolio.stockItems) {
        const stocksInHolding: PortfolioStockItem = this.portfolio.stockItems.find(stockItem => stockItem.id === id);
@@ -98,6 +127,11 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+  @param {string} units Number of units of stock user wants to sell. 
+  @param {string} id This is the id field in the PortfolioStockItem interface. Identifies stock transaction.
+  @description Checks that units is valid and user holds enough of that stock, and dispatches sell action. 
+   */
   sell(units: string, id: string) {
     if (this.isNumber(units) && this.enoughStock(id, parseInt(units, 10), this.tickerSymbol)) {
       const stock: PortfolioPayload = {
