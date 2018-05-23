cbf55347bf787f19ff4cd20593aef3694615018c
Baggio Wong <baggio.wong@rangle.io>
2018-05-23 12:34:36 -0400

    Fix linting errors.

--- src/app/containers/stock-details-page/stock-details-page.component.ts
+++ src/app/containers/stock-details-page/stock-details-page.component.ts
@@ -31,7 +31,7 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
 
   /**
    *  @param {string} tickerSymbol
-   *  @description See if stock exists in stocks array. 
+   *  @description See if stock exists in stocks array.
    */
   private stockExists(tickerSymbol: string) {
     const stockIndex = this.stocks.findIndex(stock => stock.name === tickerSymbol.toUpperCase());
@@ -40,7 +40,7 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
   }
 
   /**
-   *  @description Set up tickerSymbol (router param), stocks, stock, portfolio, currentHoldings, purchaseCost, quantity class variables. 
+   *  @description Set up tickerSymbol (router param), stocks, stock, portfolio, currentHoldings, purchaseCost, quantity class variables.
    */
   ngOnInit() {
     const subscriptionParams = this.activatedRoute.params.subscribe(params => {
@@ -60,7 +60,9 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
         this.portfolio = portfolio;
         this.currentHoldings = portfolio.stockItems;
 
-        this.purchaseCost = portfolio.stockItems.reduce((total: number, curr: PortfolioStockItem) => total + (curr.pricePerUnitOnPurchaseDate * curr.units), 0);
+        this.purchaseCost = portfolio.stockItems.reduce((total: number, curr: PortfolioStockItem) => {
+          return total + (curr.pricePerUnitOnPurchaseDate * curr.units);
+        }, 0);
         this.quantity = portfolio.stockItems.reduce((total: number, curr: PortfolioStockItem) => total + curr.units, 0);
       });
 
@@ -71,7 +73,7 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
   }
 
   /**
-   *  @description Unsubscribe to subscriptions. 
+   *  @description Unsubscribe to subscriptions.
    */
   ngOnDestroy() {
     for (const subscription of this.subscriptions) {
@@ -83,7 +85,7 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
 
   /**
    *  @param {string} input
-   *  @description Checks if string is an integer. 
+   *  @description Checks if string is an integer.
    */
   private isNumber(input: string) {
     const numberRegex = new RegExp(/^\d+$/, 'g');
@@ -91,9 +93,9 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
   }
 
   /**
-   *  @param {string} units Number of units of stock user wants to buy. 
+   *  @param {string} units Number of units of stock user wants to buy.
    *  @description If purchase units is an integer, dispatch buy action. Otherwise shows error message (invalid input).
-   */ 
+   */
   buy(units: string) {
     if (this.isNumber(units)) {
       this.showBuyError = false;
@@ -115,9 +117,10 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
 
   /**
    @param {string} id This is the id field in the PortfolioStockItem interface. Identifies stock transaction.
-   @param {number} targetUnits Number of units of stock user wants to sell. 
+   @param {number} targetUnits Number of units of stock user wants to sell.
    @param {string} tickerSymbol
-   @description Checks if user has enough units of stock to sell. This only checks for stocks under the provided id, which means that other transactions made for the same stock will be ignored. 
+   @description Checks if user has enough units of stock to sell. This only checks for stocks under the provided id,
+   which means that other transactions made for the same stock will be ignored.
    */
   private enoughStock(id: string, targetUnits: number, tickerSymbol: string) {
     if (this.portfolio.stockItems) {
@@ -128,9 +131,9 @@ export class StockDetailsPageComponent implements OnInit, OnDestroy {
   }
 
   /**
-  @param {string} units Number of units of stock user wants to sell. 
+  @param {string} units Number of units of stock user wants to sell.
   @param {string} id This is the id field in the PortfolioStockItem interface. Identifies stock transaction.
-  @description Checks that units is valid and user holds enough of that stock, and dispatches sell action. 
+  @description Checks that units is valid and user holds enough of that stock, and dispatches sell action.
    */
   sell(units: string, id: string) {
     if (this.isNumber(units) && this.enoughStock(id, parseInt(units, 10), this.tickerSymbol)) {
